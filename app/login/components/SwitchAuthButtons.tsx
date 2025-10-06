80c0206d30f6677d7f39613c349815fa679c2a4b
mostafaelfawal <armostafa982@gmail.com>
2025-10-06 23:43:38 +0300

    remove forget password

--- app/login/components/SwitchAuthButtons.tsx
+++ app/login/components/SwitchAuthButtons.tsx
@@ -1,15 +1,11 @@
 "use client";
 
-import forgetPassword from "../utils/forgetPassword";
-
 export default function SwitchAuthButtons({
   inLogin,
   setInLogin,
-  email,
 }: {
   inLogin: boolean;
   setInLogin: (v: boolean) => void;
-  email: string;
 }) {
   return (
     <div className="flex w-full max-w-sm justify-between text-sm mt-4">
@@ -20,10 +16,7 @@ export default function SwitchAuthButtons({
         {inLogin ? "إنشاء حساب جديد" : "العودة لتسجيل الدخول"}
       </button>
       {inLogin && (
-        <button
-          onClick={() => forgetPassword(email)}
-          className="hover:underline text-main font-bold"
-        >
+        <button className="hover:underline text-main font-bold">
           نسيت كلمة المرور؟
         </button>
       )}
